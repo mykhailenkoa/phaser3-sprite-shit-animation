0c8cadcce3e4c0ef82b5660089d9517a01a7d2e4
mykhailenkoa <59391219+mykhailenkoa@users.noreply.github.com>
2023-05-11 18:25:05 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# phaser3-sprite-shit-animation
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/phaser3-typescript-playground-4ga6yr)
---
+++ src/PhaserExample.ts
@@ -0,0 +1,37 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: '#2d2d2d',
+    parent: 'phaser-example',
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+export var game = new Phaser.Game(config);
+
+export function preload ()
+{
+    var t = this as Phaser.Scene;
+    t.load.spritesheet('diamonds', 'assets/sprites/diamonds32x24x5.png', { frameWidth: 32, frameHeight: 24 });
+}
+
+function create ()
+{
+    var group = this.add.group({
+        key: 'diamonds',
+        frame: [ 0, 1, 2, 3, 4 ],
+        frameQuantity: 20
+    });
+
+    Phaser.Actions.GridAlign(group.getChildren(), {
+        width: 10,
+        height: 10,
+        cellWidth: 32,
+        cellHeight: 32,
+        x: 100,
+        y: 100
+    });
+}
